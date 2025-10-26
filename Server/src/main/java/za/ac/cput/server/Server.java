343f2249aa7ac0b9696f0102f22406f24f26ef64
user <user@192.168.3.101>
2025-10-26 09:51:45 +0200

    Server processClient almost complete missing the admin student password check CreateDB complete AdminDAO complete Matthew Ferreira

--- Server/src/main/java/za/ac/cput/server/Server.java
+++ Server/src/main/java/za/ac/cput/server/Server.java
@@ -5,6 +5,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import za.ac.cput.DAO.CourseDAO;
+import za.ac.cput.DAO.EnrolmentDAO;
+import za.ac.cput.DAO.StudentDAO;
+import za.ac.cput.domain.Course;
+import za.ac.cput.domain.Enrolment;
+import za.ac.cput.domain.Admin;
+import za.ac.cput.domain.Student;
 
 public class Server {
 
@@ -24,19 +32,19 @@ public class Server {
 
     public void listen() {
         try {
-        System.out.println("Server is listening");
-        client = listener.accept();
-        System.out.println("Now we processClient");
-        processClient();
-    }catch(IOException ioe){
-            System.out.println("IO Exception found:"+ioe.getMessage());
-    }
+            System.out.println("Server is listening");
+            client = listener.accept();
+            System.out.println("Now we processClient");
+            processClient();
+        } catch (IOException ioe) {
+            System.out.println("IO Exception found:" + ioe.getMessage());
+        }
     }
 
     private void getStreams() throws IOException {
- 
-            out = new ObjectOutputStream(client.getOutputStream());
-            in = new ObjectInputStream(client.getInputStream());
+
+        out = new ObjectOutputStream(client.getOutputStream());
+        in = new ObjectInputStream(client.getInputStream());
 
     }
 
@@ -53,21 +61,58 @@ public class Server {
     public void processClient() {
         try {
             getStreams();
-            do{
-               
-            }while(true);
 
-        }catch(IOException ioe){
-            System.out.println("IO Exception:"+ioe.getMessage());
-        }finally{
+            Object request = in.readObject();
+
+            do {
+                //can just call the object: 
+                //Student student = (Student) in.readObject;
+                //...
+                //Courses courses = (Courses) in.readObject;
+                //...
+                //Enrolment enrolment = (Enrolment) in.readObject;
+                //...
+                //but then client needs to do everything in this order exactly otherwise ClassCastException gets thrown and everything breaks
+
+                if (request instanceof Student) {
+                    Student student = (Student) request;
+                    StudentDAO dao = new StudentDAO();
+                    dao.addStudent(student);
+
+                } else if (request instanceof Course) {
+                    Course course = (Course) request;
+                    CourseDAO dao = new CourseDAO();
+                    dao.addCourse(course);
+
+                    ArrayList<Course> list = dao.getAllCourses();
+                    out.writeObject(list);
+                    out.flush();
+
+                } else if (request instanceof Enrolment) {
+                    Enrolment enrolment = (Enrolment) request;
+                    EnrolmentDAO dao = new EnrolmentDAO();
+                    dao.addEnrolment(enrolment);
+
+                    ArrayList<Enrolment> list = dao.getAllEnrolments();
+                    out.writeObject(list);
+
+//                } else if (request instanceof Admin) {
+//                    Admin l = (Admin) request;
+//
+                }
+
+            } while (true);
+
+        } catch (IOException | ClassNotFoundException ioe) {
+            System.out.println("IO Exception:" + ioe.getMessage());
+        } finally {
             closeAll();
         }
     }
-    
 
     public static void main(String[] args) {
-            Server server = new Server();
-            server.listen();
-        
+        Server server = new Server();
+        server.listen();
+
     }
 }
